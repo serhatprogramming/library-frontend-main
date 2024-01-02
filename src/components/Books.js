7e8c6a16b3911315779b5efedcc8c4d0b51f1822
serhatprogramming <serhatprogramming@gmail.com>
2024-01-01 20:13:26 -0500

    all books query added and used.

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,18 @@
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries";
+import {} from "@apollo/client";
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS);
+
   if (!props.show) {
-    return null
+    return null;
   }
 
-  const books = []
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+  const books = result.data.allBooks;
 
   return (
     <div>
@@ -26,7 +35,7 @@ const Books = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
